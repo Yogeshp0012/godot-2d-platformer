11c4189ffd55c5a88eedfc26b9810886750b38ca
Yogeshp0012 <yogeshp0012@gmail.com>
2022-06-18 16:03:37 +0530

    Add movement for player

--- src/Actors/Actor.gd
+++ src/Actors/Actor.gd
@@ -1,15 +1,12 @@
 extends KinematicBody2D
 class_name Actor
 
+const FLOOR_NORMAL = Vector2.UP;
+
 export var speed:= Vector2(300.0 , 1000.0);
 
 export var gravity: = 3500.0;
 
-var velocity: = Vector2.ZERO;
-
-# To give movement to the player
-func _physics_process(delta: float) -> void:
-	velocity.y += gravity * delta;
-	velocity.y =  min(speed.y, velocity.y); 
-	velocity = move_and_slide(velocity);
+var _velocity: = Vector2.ZERO;
+	
 	
